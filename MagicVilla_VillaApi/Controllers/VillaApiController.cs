8d9800650e50399a801647334f801e71feef18d9
Muhammed Roshan <mroshan74@gmail.com>
2023-03-01 10:11:45 +0530

    Basic CR of CRUD, validation and status return using ActionResult and documentations

---
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -0,0 +1,72 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers;
+
+// [Route("api/[controller]")]
+[Route("api/villas")]
+[ApiController]
+public class VillaApiController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+    {
+        return Ok(VillaStore.villaList);
+    }
+    
+    // [HttpGet("id")]
+    // [ProducesResponseType(200, Type = typeof(VillaDTO)]
+    [HttpGet("{id:int}", Name = "GetVilla")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<VillaDTO> GetVilla(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+        if (villa == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(villa);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDto)
+    {
+        // if (!ModelState.IsValid)
+        // {
+        //     return BadRequest(ModelState);
+        // }
+        if (VillaStore.villaList.FirstOrDefault(v => v.Name.ToLower() == villaDto.Name.ToLower()) != null)
+        {
+            ModelState.AddModelError("CustomError", "Villa already exist");
+            return BadRequest(ModelState);
+        }
+        if (villaDto is null)
+        {
+            return BadRequest(villaDto);
+        }
+
+        if (villaDto.Id < 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+        VillaStore.villaList.Add(villaDto);
+
+        return CreatedAtRoute("GetVilla",new { id = villaDto.Id }, villaDto);
+    }
+}
---
+++ MagicVilla_VillaApi/Models/Villa.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla_VillaApi.Models;
+
+public class Villa
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
